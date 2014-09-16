f761a53860a116b4dc23382ce937c7c2e987cebd
Fadie Hannona <fadiehannona@gmail.com>
2014-09-16 12:13:41 +0100

    3rd commit, Refactored previous code

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,21 +1,14 @@
 class Bike
 
-	# the initalize method is always called when you create a new
-	# class by typing Bike.new
 	def initialize
-		# all instance variables begin with @
-		# this must be an instance variable because we'll need it
-		# in other methods
-		@broken = false
+		fix!()
 	end
 
 	def broken?
-		# instance variables are accessible in all methods
 		@broken
 	end
 
 	def break!
-		# and any instance method can update them
 		@broken = true
 	end
 
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,26 +1,20 @@
-require 'bike'
+require './lib/bike'
 
-# we're describing the functionality of a specific class, Bike
 describe Bike do
-	# This is a specific feature (behaviour)
-	# That we expect to be present
+
+	let(:bike) { Bike.new }
 it "should not be broken after we create it" do
-	the_bike = Bike.new # init new object of Bike class
-	# expect an instance of this class to have
-	# a method "broken?" that should return false
-	expect(the_bike).not_to be_broken
+	expect(bike).not_to be_broken
 end
 
 it "should be able to break" do
-	bike = Bike.new
 	bike.break!
 	expect(bike).to be_broken
 end
 
 it "should be able to get fixed" do
-		bike = Bike.new
-		bike.break!
-		bike.fix!
-		expect(bike).not_to be_broken
+	bike.break!
+	bike.fix!
+	expect(bike).not_to be_broken
 end
 end
