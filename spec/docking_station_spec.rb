50e990555104a6819fc30b4db7fc2ae93b9defb9
Fadie Hannona <fadiehannona@gmail.com>
2014-09-16 13:47:38 +0100

    Added release method in docking station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,11 +3,18 @@ require './lib/bike'
 
 describe DockingStation do
 
+	let(:bike) { Bike.new }
+	let(:station) { DockingStation.new }
+
 	it "Should accept a bike" do
-	bike = Bike.new
-	station = DockingStation.new
-	expect(station.bike_count).to eq(0)
-	station.dock(bike)
-	expect(station.bike_count).to eq(1)
+		expect(station.bike_count).to eq(0)
+		station.dock(bike)
+		expect(station.bike_count).to eq(1)
+	end
+
+	it "Should release a bike" do
+		station.dock(bike)
+		station.release(bike)
+		expect(station.bike_count).to eq(0)
 	end
 end
