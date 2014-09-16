3893fb48794e6ef5acf37de394f658313c100f80
Fadie Hannona <fadiehannona@gmail.com>
2014-09-16 11:57:57 +0100

    Added fix. 2nd commit.

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,18 +4,23 @@ require 'bike'
 describe Bike do
 	# This is a specific feature (behaviour)
 	# That we expect to be present
-	it "should not be broken after we create it" do
-		the_bike = Bike.new # init new object of Bike class
-		# expect an instance of this class to have
-		# a method "broken?" that should return false
-		expect(the_bike).not_to be_broken
-	end
+it "should not be broken after we create it" do
+	the_bike = Bike.new # init new object of Bike class
+	# expect an instance of this class to have
+	# a method "broken?" that should return false
+	expect(the_bike).not_to be_broken
 end
 
-describe Bike do
 it "should be able to break" do
 	bike = Bike.new
 	bike.break!
 	expect(bike).to be_broken
+end
+
+it "should be able to get fixed" do
+		bike = Bike.new
+		bike.break!
+		bike.fix!
+		expect(bike).not_to be_broken
 end
 end
