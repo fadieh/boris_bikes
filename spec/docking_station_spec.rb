267a8af7d45a8f0d239e1982e6cc25a9673cc93d
Fadie Hannona <fadiehannona@gmail.com>
2014-09-16 13:39:38 +0100

    Added Docking Station rb and rspec

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,13 @@
+require './lib/docking_station'
+require './lib/bike'
+
+describe DockingStation do
+
+	it "Should accept a bike" do
+	bike = Bike.new
+	station = DockingStation.new
+	expect(station.bike_count).to eq(0)
+	station.dock(bike)
+	expect(station.bike_count).to eq(1)
+	end
+end
